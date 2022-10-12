3792e27157dd0f151716a57cf8d774e74a60a8b4
Gylos <giltheod@gmail.com>
2022-10-12 23:53:58 +0300

    completed

---
+++ util/auth.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const API_KEY = "AIzaSyCKGCuLGyq7QKBF2M4K2YRE_ErJ-GtQBt8";
+
+const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+  const token = response.data.idToken;
+  return token;
+};
+
+export const createUser = (email, password) => {
+  return authenticate("signUp", email, password);
+};
+
+export const login = (email, password) => {
+  return authenticate("signInWithPassword", email, password);
+};
